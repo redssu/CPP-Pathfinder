2dcc5ac328dcd4060036bc681bad458a3adb0345
Redssu <hey@redssu.pl>
2022-02-17 08:25:34 +0100

    Dodanie klasy Heap - Sterty

---
+++ lib/Heap/Heap.cpp
@@ -0,0 +1,68 @@
+#include <vector>
+#include "Heap.hpp"
+
+template <class T> void Heap<T>::Swap ( int index1, int index2 ) {
+    if ( index1 < 0 || index1 >= heap.size() || index2 < 0 || index2 >= heap.size() ) {
+        throw "Heap::Swap: Indeks elementu jest nieprawidłowy";
+    }
+
+    T temp = heap[ index1 ];
+    heap[ index1 ] = heap[ index2 ];
+    heap[ index2 ] = temp;
+}
+
+template <class T> void Heap<T>::FixHeap ( int index ) {
+    int lowestIndex = index;
+
+    int leftChildIndex = lowestIndex * 2 + 1;
+    int rightChildIndex = lowestIndex * 2 + 2;
+
+
+    if ( leftChildIndex < heap.size() && heap[ lowestIndex ] > heap[ leftChildIndex ] ) {
+        lowestIndex = leftChildIndex;
+    }
+
+    if ( rightChildIndex < heap.size() && heap[ lowestIndex ] > heap[ rightChildIndex ] ) {
+        lowestIndex = rightChildIndex;
+    }
+
+    if ( lowestIndex != index ) {
+        Swap ( index, lowestIndex );
+        FixHeap ( lowestIndex );
+    }
+}
+
+template <class T> void Heap<T>::Insert ( T element ) {
+    heap.push_back ( element );
+
+    int index = heap.size() - 1;
+    int parent = index / 2;
+
+    while ( index > 0 && heap[ index ] < heap[ parent ] ) {
+        Swap ( parent, index );
+        index = parent;
+    }
+}
+
+template <class T> T Heap<T>::GetTop () {
+    if ( heap.size() == 0 ) {
+        throw "Heap::GetTop: Pusta sterta";
+    }
+
+    return heap[ 0 ];
+}
+
+template <class T> void Heap<T>::Pop () {
+    if ( heap.size() == 0 ) {
+        throw "Heap::Pop: Pusta sterta";
+    }
+
+    heap[ 0 ] = heap[ heap.size() - 1 ];
+    heap.pop_back();
+
+    FixHeap ( 0 );
+}
+
+template <class T> int Heap<T>::Size () {
+    return heap.size();
+}
---
+++ lib/Heap/Heap.hpp
@@ -0,0 +1,58 @@
+#ifndef Heap_HPP
+#define Heap_HPP
+#include <vector>
+
+template <class T>
+class Heap {
+    private:
+        /// @brief Sterta
+        std::vector<T> heap;
+
+        /**
+         * @brief Zamienia miejscami dwa elementy w stercie
+         * 
+         * @param a Indeks pierwszego elementu
+         * @param b Indeks drugiego elementu
+         * @throw "Heap::Swap: Indeks elementu jest nieprawidłowy"
+         */
+        void Swap ( int index1, int index2 );
+
+        /**
+         * @brief Naprawa sterty
+         * 
+         * @param index Indeks elementu, od którego rozpocząć naprawę
+         */
+        void FixHeap ( int index );
+
+    public:
+        /**
+         * @brief Dodaje nowy element do sterty
+         * 
+         * @param element Element do dodania
+         */
+        void Insert ( T element );
+
+        /**
+         * @brief Zwraca ze sterty najwyżej położony element
+         * 
+         * @note Tu fajnie byłoby mieć wskaźnik, ale nie wykryjemy zmiany zmiennej
+         *     a poza tym vector często jest przenoszony w pamięci, więc wskaźniki
+         *     często są nieaktualne
+         * @return Najwyżej położony element
+         */
+        T GetTop ();
+
+        /**
+         * @brief Usuwa najwyższy element ze sterty
+         */
+        void Pop ();
+
+        /**
+         * @brief Zwraca rozmiar sterty
+         * 
+         * @return Rozmiar sterty
+         */
+        int Size ();
+};
+
+#endif
