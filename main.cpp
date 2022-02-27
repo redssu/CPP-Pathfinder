50bff9078930a2470552c20f5039bfcc0d87e572
Redssu <hey@redssu.pl>
2022-02-27 20:31:42 +0100

    Dodanie wyświetlanie drogi w mapie

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include "lib/Console/Console.hpp"
 #include "lib/MapPoint/MapPoint.hpp"
 #include "lib/Map/Map.hpp"
@@ -12,7 +13,25 @@ int main () {
     try {
         MapLoader mapLoader = MapLoader( "map.txt" );
         mapLoader.Tokenize();
-        mapLoader.Interpret();
+        Map map = mapLoader.Interpret();
+
+        PathFinder pathFinder = PathFinder( &map );
+
+        std::vector<MapPoint*> path = pathFinder.FindPath();
+        int pathSize = path.size();
+
+        for ( int i = 0; i < pathSize; i++ ) {
+            MapPoint* point = map.GetMapPoint( path[ i ]->X(), path[ i ]->Y() );
+
+            if ( point->Symbol() == 'E' || point->Symbol() == 'S' ) {
+                continue;
+            }
+
+            point->SetSymbol( '*' );
+            point->SetFGColor( RED );
+        }
+
+        map.Print();
     }
     catch ( PathFinderException exception ) {
         Console::SetColor( BRIGHT_WHITE, BRIGHT_RED );
